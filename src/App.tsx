8b1c0fb37f35099afee8deaf880a14a173a19257
LucaP97 <luca.pike7@gmail.com>
2023-08-21 18:04:56 +0100

    passing props via functions

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Alert from "./components/Alert";
+
+function App() {
+  return (
+    <div className="alert alert-primary">
+      <Alert>
+        Hello <h1>World</h1>
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
